c3bba67f9aa9282b1bbc20566b8fb9aeb68c795b
Aahmed <ammad.ahmed1111@gmail.com>
2022-08-04 13:45:15 +0500

    Uploading project

---
+++ src/components/SpinNer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="../assets/loadinf.gif" alt="" class="d-block m-auto">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SpinNer"
+}
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+//Fontawesome icon
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+
+
+// Bootstrap Css and js
+
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+//main css file
+import './style.css';
 createApp(App).use(store).use(router).mount('#app')
